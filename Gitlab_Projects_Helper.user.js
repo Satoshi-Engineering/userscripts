fdad55445c75a16e7e0a0f8be2b98849666d49e8
David Scheuch <dave@satoshiengineering.com>
2022-06-27 17:12:08 +0200

    New userscript Gitlab projects helper

---
+++ Gitlab_Projects_Helper.user.js
@@ -0,0 +1,50 @@
+// ==UserScript==
+// @name         Gitlab projects helper
+// @namespace    https://satoshiengineering.com/
+// @version      0.1
+// @description  Add links to directly filter for our project labels
+// @author       dr-erych
+// @match        https://gitlab.satoshiengineering.com/satoshiengineering/projects/-/boards/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=satoshiengineering.com
+// @grant        none
+// ==/UserScript==
+
+(async function() {
+    'use strict'
+
+    const csrfToken = document.querySelector('meta[name=csrf-token]').content
+
+    const res = await fetch('/api/graphql', {
+        headers: {
+            'content-type': 'application/json',
+            'x-csrf-token': csrfToken,
+        },
+        body: "[{\"operationName\":\"projectLabels\",\"variables\":{\"fullPath\":\"satoshiengineering/projects\",\"searchTerm\":\"\"},\"query\":\"query projectLabels($fullPath: ID!, $searchTerm: String) {\\n  workspace: project(fullPath: $fullPath) {\\n    id\\n    labels(searchTerm: $searchTerm, includeAncestorGroups: true) {\\n      nodes {\\n        ...Label\\n        __typename\\n      }\\n      __typename\\n    }\\n    __typename\\n  }\\n}\\n\\nfragment Label on Label {\\n  id\\n  title\\n  description\\n  color\\n  textColor\\n  __typename\\n}\\n\"}]",
+        method: 'POST',
+        mode: 'cors',
+        credentials: 'include',
+    })
+
+    const json = await res.json()
+
+    const labels = json[0].data.workspace.labels.nodes
+        .filter(({ color }) => color === '#808080')
+        .map(({ title }) => title)
+
+    console.log(labels, json[0].data.workspace.labels.nodes)
+
+    const parentEl = document.querySelector('.breadcrumbs-container')
+
+    labels.forEach(label => {
+        const html = `<span class="gl-label js-no-trigger gl-label-sm gl-label-text-light" style="--label-background-color:#808080; --label-inset-border:inset 0 0 0 1px #808080; margin-right: 4px;">
+         <a href="#" class="gl-link gl-label-link"><span class="gl-label-text">
+             ${label}
+         </span></a></span>`
+        const labelEl = new DOMParser().parseFromString(html, 'text/html').body.childNodes[0]
+        parentEl.appendChild(labelEl)
+        const labelLink = labelEl.querySelector('a')
+        labelLink.href = `${location.origin}${location.pathname}?label_name[]=${label}`
+    })
+
+
+})()
