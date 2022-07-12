1304887686b34b3be0f7ad55b42ab90a112c1b1f
David <david.rerych@gmail.com>
2022-07-12 18:06:49 +0200

    Update Redis_Commander_JSON_Helper.user.js

--- Redis_Commander_JSON_Helper.user.js
+++ Redis_Commander_JSON_Helper.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         Redis Commander JSON Helper
 // @namespace    https://github.com/Satoshi-Engineering/userscripts
-// @version      0.4
+// @version      0.5
 // @description  Help you editing JSONs in Redis Commander (https://joeferner.github.io/redis-commander/)
 // @author       https://github.com/dr-erych
 // @icon         https://joeferner.github.io/redis-commander/favicon.png
 // @grant        none
 // ==/UserScript==
 
-(function() {
+;(function() {
 
     const flattenKeys = (obj, currentRoot = undefined) => {
         return Object.keys(obj).map(key => {
@@ -23,7 +23,7 @@
 
     const deepValue = (o, p) => p.split('.').reduce((a, v) => a[v], o)
 
-    const getJsonPathFromTextareaSelection = (textarea) => {
+    const getValueForPath = (jsonPath) => {
         let object
         try {
             object = JSON.parse(textarea.value)
@@ -31,52 +31,82 @@
             throw new Error(error)
             return
         }
-        const selectedText = textarea.value.substring(textarea.selectionStart, textarea.selectionEnd)
-        let jsonPath
-        if (selectedText != null && selectedText.length > 0) {
-            const occurrence = textarea.value.substring(0, textarea.selectionStart).match(new RegExp(`\"${selectedText}\"\\s*:`, 'g'))?.length || 0
-            const pathsToKey = flattenKeys(object).filter(path => path.endsWith(selectedText))
-            jsonPath = pathsToKey[occurrence]
+        try {
+            return deepValue(object, jsonPath)
+        } catch (error) {
+            return undefined
         }
-        return { object, jsonPath }
     }
 
-    let currentlyInitialized = undefined
-
-    const init = () => {
-        const itemDataEl = document.querySelector('#body #itemData')
-
-        if (currentlyInitialized === itemDataEl) {
+    const getJsonPathFromTextareaSelection = (textareaInternal) => {
+        if (textareaInternal == null && textarea != null) {
+            textareaInternal = textarea
+        }
+        let object
+        try {
+            object = JSON.parse(textareaInternal.value)
+        } catch (error) {
+            throw new Error(error)
             return
         }
-        currentlyInitialized = itemDataEl
-
-        const textarea = itemDataEl?.querySelector('#stringValue')
-        if (itemDataEl == null || itemDataEl.querySelector('#editStringForm') == null || textarea == null || itemDataEl.querySelector('#saveKeyButton') != null) {
-            return
+        const selectedText = textareaInternal.value.substring(textareaInternal.selectionStart, textareaInternal.selectionEnd)
+        let jsonPath
+        if (selectedText != null && selectedText.length > 0) {
+            const occurrence = textareaInternal.value.substring(0, textareaInternal.selectionStart).match(new RegExp(`\"${selectedText}\"\\s*:`, 'g'))?.length || 0
+            const pathsToKey = flattenKeys(object).filter(path => path === selectedText || path.endsWith(`.${selectedText}`))
+            jsonPath = pathsToKey[occurrence]
         }
+        return { object, jsonPath, value: deepValue(object, jsonPath) }
+    }
 
-        const redisKey = [...itemDataEl.querySelectorAll(':scope > label')].find(el => el.textContent.startsWith('Key: ')).querySelector('b').textContent.trim()
+    let currentlyInitialized
+    let itemDataEl
+    let textarea
+    let redisKey
+    let commandInput
 
-        const commandInput = document.querySelector('#commandLine #_readline_input')
+    let editorReadyListeners = []
 
+    const addButton = (label, title, onClick) => {
         const button = document.createElement('button')
-        button.textContent = 'JSON.SET 👇'
-        button.title = 'Select a key of the JSON in the textarea, then click this button'
+        button.textContent = label
+        button.title = title
         itemDataEl.appendChild(button)
         itemDataEl.appendChild(document.createTextNode(' '))
 
         button.addEventListener('click', (event) => {
             event.preventDefault()
             button.style.borderColor = null
-            let jsonPath, object
             try {
-                ({ jsonPath, object } = getJsonPathFromTextareaSelection(textarea))
+                onClick.call(undefined, { redisKey, textarea, commandInput })
             } catch (error) {
+                console.error(error)
                 button.style.borderColor = 'red'
             }
+        })
+    }
+
+    const init = () => {
+        itemDataEl = document.querySelector('#body #itemData')
+
+        if (currentlyInitialized === itemDataEl) {
+            return
+        }
+        currentlyInitialized = itemDataEl
+
+        textarea = itemDataEl?.querySelector('#stringValue')
+        if (itemDataEl == null || itemDataEl.querySelector('#editStringForm') == null || textarea == null || itemDataEl.querySelector('#saveKeyButton') != null) {
+            return
+        }
+
+        redisKey = [...itemDataEl.querySelectorAll(':scope > label')].find(el => el.textContent.startsWith('Key: ')).querySelector('b').textContent.trim()
+
+        commandInput = document.querySelector('#commandLine #_readline_input')
+
+        addButton('JSON.SET 👇', 'Select a key of the JSON in the textarea, then click this button', ({ redisKey, textarea, commandInput }) => {
+            let { jsonPath, object, value } = getJsonPathFromTextareaSelection(textarea)
             let quotes = ''
-            if (jsonPath == null || typeof deepValue(object, jsonPath) === 'string') {
+            if (jsonPath == null || typeof value === 'string') {
                 quotes = '""'
             }
             const output = `json.set ${redisKey} $.${jsonPath || ''} '${quotes}'`
@@ -85,20 +115,11 @@
             commandInput.setSelectionRange(output.length - quotes.length / 2 - 1, output.length - quotes.length / 2 - 1)
         })
 
-        const buttonSetNull = document.createElement('button')
-        buttonSetNull.textContent = 'JSON.SET null 👇'
-        buttonSetNull.title = 'Select a key of the JSON in the textarea, then click this button'
-        itemDataEl.appendChild(buttonSetNull)
-        itemDataEl.appendChild(document.createTextNode(' '))
-
-        buttonSetNull.addEventListener('click', (event) => {
-            event.preventDefault()
-            button.style.borderColor = null
-            let jsonPath, object
-            try {
-                ({ jsonPath, object } = getJsonPathFromTextareaSelection(textarea))
-            } catch (error) {
-                button.style.borderColor = 'red'
+        addButton('JSON.SET null 👇', 'Select a key of the JSON in the textarea, then click this button', ({ redisKey, textarea, commandInput }) => {
+            let { jsonPath, object } = getJsonPathFromTextareaSelection(textarea)
+            let quotes = ''
+            if (jsonPath == null || typeof deepValue(object, jsonPath) === 'string') {
+                quotes = '""'
             }
             commandInput.value = `json.set ${redisKey} $.${jsonPath || ''} 'null'`
             commandInput.focus()
@@ -106,22 +127,7 @@
 
         itemDataEl.appendChild(document.createElement('br'))
         itemDataEl.appendChild(document.createElement('br'))
-        const buttonConvertTimestamp = document.createElement('button')
-        buttonConvertTimestamp.textContent = 'Milliseconds to Date ⏰'
-        buttonConvertTimestamp.title = 'Select a timestamp value in the textarea, then click this button'
-        itemDataEl.appendChild(buttonConvertTimestamp)
-        itemDataEl.appendChild(document.createTextNode(' '))
-        const dateContainer = document.createElement('span')
-        const inputTimestamp = document.createElement('input')
-        const inputDate = document.createElement('input')
-        dateContainer.appendChild(inputTimestamp)
-        dateContainer.appendChild(document.createTextNode(' <-> '))
-        dateContainer.appendChild(inputDate)
-        itemDataEl.appendChild(dateContainer)
-        itemDataEl.appendChild(document.createElement('br'))
-        dateContainer.style.display = 'none'
-        buttonConvertTimestamp.addEventListener('click', (event) => {
-            event.preventDefault()
+        addButton('Milliseconds to Date ⏰', 'Select a timestamp value in the textarea, then click this button', ({ redisKey, textarea, commandInput }) => {
             dateContainer.style.display = 'inline'
             const tsString = textarea.value.substring(textarea.selectionStart, textarea.selectionEnd).trim()
             if (tsString.length < 1) {
@@ -131,8 +137,23 @@
             inputTimestamp.value = Number(tsString)
             inputDate.value = new Date(Number(inputTimestamp.value)).toISOString()
         })
+        const dateContainer = document.createElement('span')
+        const inputTimestamp = document.createElement('input')
+        const inputDate = document.createElement('input')
+        dateContainer.appendChild(inputTimestamp)
+        dateContainer.appendChild(document.createTextNode(' <-> '))
+        dateContainer.appendChild(inputDate)
+        itemDataEl.appendChild(dateContainer)
+        itemDataEl.appendChild(document.createElement('br'))
+        dateContainer.style.display = 'none'
         inputDate.addEventListener('input', ({ currentTarget }) => { inputTimestamp.value = new Date(currentTarget.value).getTime() })
         inputTimestamp.addEventListener('input', ({ currentTarget }) => { inputDate.value = new Date(Number(currentTarget.value)).toISOString() })
+
+        itemDataEl.appendChild(document.createElement('br'))
+        itemDataEl.appendChild(document.createElement('br'))
+
+        editorReadyListeners.forEach(listener => listener.call(undefined, { addButton, getValueForPath, getJsonPathFromTextareaSelection }))
+
     }
 
     const observer = new MutationObserver((mutations) => {
@@ -149,5 +170,7 @@
     }
 
     observer.observe(document.querySelector('#body'), { childList: true })
+
+    window.REDIS_COMMANDER_JSON_HELPER_EXTEND = (onEditorReady) => { editorReadyListeners = [...editorReadyListeners, onEditorReady] }
 
 })()
